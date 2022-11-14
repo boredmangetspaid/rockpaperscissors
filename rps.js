cd71486416ff598c541e59a91eaf8711d9815e92
boredmangetspaid <develop202234@gmail.com>
2022-11-14 17:49:21 -0500

    added random generated computer choice, playRound function

--- rps.js
+++ rps.js
@@ -1,1 +1,27 @@
-console.log('hi')
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    if (choice == 1) {
+        return 'rock';
+    } else if (choice == 2) {
+        return 'paper'
+    } else if (choice == 3) {
+        return "scissor"
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if(playerSelection=="rock"&&computerSelection=="scissor"){
+        return 'win';
+    }else if(playerSelection=="rock"&&computerSelection=="paper"){
+        return 'lose';
+    }else if(playerSelection=="scissor"&&computerSelection=="paper"){
+        return 'win';
+    }else if (playerSelection=="scissor"&&computerSelection=="rock"){
+        return "lose";
+    }else if(playerSelection=="paper"&&computerSelection=="rock"){
+        return "win";
+    }else if (playerSelection=="paper"&&computerSelection=="scissor"){
+        return "lose";
+    }else {return "tie"};
+}
