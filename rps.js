44657f6a8ee7e7e4f023a9d875036e2f806c926e
boredmangetspaid <develop202234@gmail.com>
2022-12-13 15:59:28 -0500

    added rounds

--- rps.js
+++ rps.js
@@ -22,25 +22,36 @@ function playRound(playerSelection, computerSelection) {
         return 'lose';
     }
 }
-    
-function displayMessage(roundResult,playerSelection,computerSelection){
+
+function displayMessage(roundResult, playerSelection, computerSelection) {
     let displayDiv = document.querySelector("div#results");
-    if(roundResult=="tie"){
-        displayDiv.innerHTML="it was a tie!"
-    } 
-    else if(roundResult=="lose"){
-        displayDiv.innerHTML= `${playerSelection} loses to ${computerSelection}`
-    }else {
-        displayDiv.innerHTML=`${playerSelection} wins against ${computerSelection}`
+    if (roundResult == "tie") {
+        displayDiv.innerHTML = "it was a tie!"
+    } else if (roundResult == "lose") {
+        displayDiv.innerHTML = `${playerSelection} loses to ${computerSelection}`
+    } else {
+        displayDiv.innerHTML = `${playerSelection} wins against ${computerSelection}`
     }
 }
-
-
-
 
 let win = 0;
 let lose = 0;
 let tie = 0;
+let totalRounds = 0;
+
+function scoreboard() {
+    let scoreboard = document.querySelector("div#scoreboard");
+    scoreboard.innerHTML = `wins ${win} , losses${lose}, ties${tie}`
+    console.log(totalRounds)
+}
+function resetScoreBoard(){
+    win= 0;
+    lose = 0;
+    tie = 0;
+    totalRounds=0;
+}
+
+
 
 let buttons = document.querySelectorAll('button');
 buttons.forEach(
@@ -48,9 +59,36 @@ buttons.forEach(
         button.addEventListener('click', (e) => {
             let playerChoice = e.target.id;
             let computerChoice = getComputerChoice();
-            let result = playRound(playerChoice,computerChoice);
-            displayMessage(result,playerChoice,computerChoice);
+            let result = playRound(playerChoice, computerChoice);
+            displayMessage(result, playerChoice, computerChoice);
+            console.log(result);
+            if (result == "win") {
+                win++;
 
+            } else if (result == "lose") {
+                lose++;
+            } else {
+                tie++;
+            }
+            scoreboard();
+            
+            totalRounds++;
+
+            if (totalRounds > 5) {
+                let scoreboard = document.querySelector("div#scoreboard");
+                
+                if (win > lose) {
+                    scoreboard.innerHTML = `Game over you win`
+                }
+                else if (win < lose) {
+                    scoreboard.innerHTML = `Game over you lose :(`;
+                } else {
+                    scoreboard.innerHTML = `Game over it was a tie!`
+                }
+            resetScoreBoard();
+               
+
+            }
         })
     }
 )
